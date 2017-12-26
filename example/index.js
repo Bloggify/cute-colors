acd3d529746408fff38b6da2086effd2f60a3238
Ionică Bizău <bizauionica@gmail.com>
2017-12-26 11:01:04 +0200

    jsdocs

--- example/index.js
+++ example/index.js
@@ -1,9 +1,18 @@
 const CuteColors = require("..")
 
+console.log(CuteColors.getPalettes())
+// => [ 'flat', 'material' ]
+
 console.log(
-    CuteColors.simplify(
+    CuteColors.getColors(
         "flat"
         // Use only half of the colors
-      , 0.9
+      , 0.5
     )
 )
+// => { pomegranate: [ '#641e16', '#922b21', '#c0392b', '#d98880', '#f2d7d5' ],
+//   alizarin: [ '#78281f', '#b03a2e', '#e74c3c', '#f1948a', '#fadbd8' ],
+//   asbestos: [ '#424949', '#616a6b', '#7f8c8d', '#b2babb', '#e5e8e8' ],
+//   ...
+//   'wet asphalt': [ '#1b2631', '#283747', '#34495e', '#85929e', '#d6dbdf' ],
+//   'midnight blue': [ '#17202a', '#212f3d', '#2c3e50', '#808b96', '#d5d8dc' ] }
